3340c4d6284694abba5d29908c8ae8eaad4f3873
Ajay Srivastava <ajaysriv3@gmail.com>
2026-04-14 04:19:28 -0700

    use strict include in makefiles.mk

--- makefiles.mk
+++ makefiles.mk
@@ -9,14 +9,14 @@
 
 BUILDTOOLS_ROOT := $(dir $(lastword $(MAKEFILE_LIST)))makefiles
 
--include $(BUILDTOOLS_ROOT)/base.mk
--include $(BUILDTOOLS_ROOT)/deps.mk
--include $(BUILDTOOLS_ROOT)/functions.mk
--include $(BUILDTOOLS_ROOT)/git.mk
--include $(BUILDTOOLS_ROOT)/help.mk
--include $(BUILDTOOLS_ROOT)/lint.mk
--include $(BUILDTOOLS_ROOT)/os.mk
--include $(BUILDTOOLS_ROOT)/project.mk
--include $(BUILDTOOLS_ROOT)/verbose.mk
+include $(BUILDTOOLS_ROOT)/base.mk
+include $(BUILDTOOLS_ROOT)/deps.mk
+include $(BUILDTOOLS_ROOT)/functions.mk
+include $(BUILDTOOLS_ROOT)/git.mk
+include $(BUILDTOOLS_ROOT)/help.mk
+include $(BUILDTOOLS_ROOT)/lint.mk
+include $(BUILDTOOLS_ROOT)/os.mk
+include $(BUILDTOOLS_ROOT)/project.mk
+include $(BUILDTOOLS_ROOT)/verbose.mk
 
 ################################################################################
