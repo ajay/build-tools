c284f21426a6ca2687de661cd54ae5a54d64acea
Ajay Srivastava <ajaysriv3@gmail.com>
2026-04-27 10:10:07 -0700

    detect OS via /etc/os-release instead of lsb_release
    lsb_release isn't installed by default on Fedora Workstation (and
    many other distros), causing every make invocation to print
    "bash: lsb_release: command not found" and resolve OS to empty.
    
    /etc/os-release is part of the systemd os-release(5) spec, ships
    with the OS itself with zero deps, exists on every modern Linux
    distro, and its ID= field is already lowercase per spec - so we
    can drop both lsb_release and tr.

--- makefiles/os.mk
+++ makefiles/os.mk
@@ -1,14 +1,16 @@
 ifndef _OS_MK_
 _OS_MK_ := 1
 
-DEPS += tr uname
+DEPS += uname
 
 ################################################################################
 
 ifeq ($(shell uname -s),Darwin)
 	OS := mac
+else ifeq (,$(wildcard /etc/os-release))
+	$(error /etc/os-release not found; cannot detect Linux distribution)
 else
-	OS := $(shell lsb_release -is | tr A-Z a-z)
+	OS := $(shell . /etc/os-release && echo "$$ID")
 endif
 
 ################################################################################
